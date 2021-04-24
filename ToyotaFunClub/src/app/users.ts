331277f4f4f596c1178fda839cfb48a209894bd4
Miraast19 <miraast18@gmail.com>
2021-04-24 14:49:18 +0500

    Your message about the commit

---
+++ ToyotaFunClub/src/app/users.ts
@@ -0,0 +1,10 @@
+import { User } from './User';
+
+export const USERS: User[] = [
+    {
+        firstName: 'Miras',
+        lastName: 'Tkishev',
+        username: 'm_tkishev',
+        password: '123456789'
+    },
+]
